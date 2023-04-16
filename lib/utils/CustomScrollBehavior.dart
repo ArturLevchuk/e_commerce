853cd63ad69712efe0733cd47a383859074420e5
ArturLevchuk <levchukarturr@gmail.com>
2023-04-16 18:48:11 +0300

    CustomScrollBehavior updated

--- lib/utils/CustomScrollBehavior.dart
+++ lib/utils/CustomScrollBehavior.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CustomScrollBehavior extends ScrollBehavior {
-  @override
-  Widget buildViewportChrome(
-      BuildContext context, Widget child, AxisDirection axisDirection) {
-    // Disable the glow effect
+  // @override
+  // Widget buildViewportChrome(
+  //     BuildContext context, Widget child, AxisDirection axisDirection) {
+  //   // Disable the glow effect
+  //   return child;
+  // }
+
+    @override
+  Widget buildOverscrollIndicator(
+      BuildContext context, Widget child, ScrollableDetails details) {
     return child;
   }
 }
